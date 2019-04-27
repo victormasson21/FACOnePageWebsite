85127eb59e54a3f5522af20dd6562cf5f5d1b097
Rev8617 <victormasson21@gmail.com>
2019-04-27 17:49:38 +0200

    Version 7 - Finishing dropdown menu
    Adding event listener to hide the dropdown menu when clicking anywhere else on the page.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var dropdownLks = document.getElementById('ddlinks')
 var dropdownBtn = document.getElementById('linksBtn')
 
-
+/* Displays the dropdown menu when clicking 'Links' */
 function menuDropsDown() {
   if (dropdownLks.style.display == 'block') {
     dropdownLks.style.display = 'none';
@@ -10,6 +10,11 @@ function menuDropsDown() {
   }
 }
 
-
-
 dropdownBtn.addEventListener('click', menuDropsDown);
+
+/* Hides the dropdown menu when clicking anywhere else on the page */
+document.addEventListener('click', function(e) {
+    if (!dropdownBtn.contains(e.target)&&!dropdownLks.contains(e.target)) {
+      dropdownLks.style.display = 'none';
+    }
+});
